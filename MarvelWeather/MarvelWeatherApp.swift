550d25dcdd26c9aad267fc1486be08f46a93eac5
Valentyn <waliok2292@gmail.com>
2023-04-18 17:19:51 +0200

    Initial Commit

---
+++ MarvelWeather/MarvelWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelWeatherApp.swift
+//  MarvelWeather
+//
+//  Created by Waliok on 18/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct MarvelWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
